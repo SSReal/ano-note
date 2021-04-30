126527264aaf41eb4819d25f00315e1c8a8e1bfb
Sajal Singhal <ss.13052001@outlook.com>
2021-04-30 10:29:00 +0530

    Add server api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React, {useState} from "react"
+import React, {useEffect, useState} from "react"
 import './App.css';
 import NavBar from "./components/NavBar"
 import Content from "./components/Content"
 import NoteControls from "./components/NoteControls"
 
 function App() {
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:9000/v1/notes").then(
+      (response) => response.json()
+    ).then((res) => setNotes(res)).catch((err) => console.log(err));
+  }, [])
+
   return (
     <div className ="App">
       <NavBar />
